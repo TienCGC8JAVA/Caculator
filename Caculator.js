1b5ae6d742e63d3159c6c582f7005c2b087fb361
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-03-28 09:03:45 +0700

    ứng dụng máy tính

--- Caculator.js
+++ Caculator.js
@@ -19,9 +19,9 @@ function multiplication()
     console.log("Tích của 2 số là: " + Mul);
 }
 
-// document.getElementById().innerHTML = ' ' + ... ; hiển thị thay đổi trên trang có sẵn
+// document.getElementById().innerHTML = ' ' + ... ; hiển thị thay đổi trên thẻ id cần thực thi và xóa content cũ trong thẻ
 function division()
 {
     let Div = parseInt(document.getElementById("num1").value) / parseInt(document.getElementById("num2").value);
-    document.getElementById("btn").innerHTML = "<br/ >" + " Thương của 2 số là: " + Div;
+    document.getElementById("btn").innerHTML = "<br />" + "<br />" + " Thương của 2 số là: " + Div;
 }
